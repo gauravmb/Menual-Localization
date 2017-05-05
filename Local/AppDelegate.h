58b5c6991529c13510312211276fbcf4806e3955
Gaurav Bhatia <gaurav.bhatia@hurix.com>
2017-05-05 17:18:56 +0530

    Initial Commit

---
+++ Local/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Local
+//
+//  Created by Gaurav Bhatia on 5/5/17.
+//  Copyright © 2017 Gaurav Bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Local/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Local
+//
+//  Created by Gaurav Bhatia on 5/5/17.
+//  Copyright © 2017 Gaurav Bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
